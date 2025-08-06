f7b170964aa79cef9add9b6fc5f0c8364a9fb9e3
grimWaffles <waziullah@live.com>
2025-08-06 12:52:44 +0600

    update: Redis Controller

---
+++ API Gateway/Models/RedisTestModel.cs
@@ -0,0 +1,10 @@
+namespace API_Gateway.Models
+{
+    public class RedisTestModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public bool IsRegistered { get; set; }
+        public DateTime RegistrationDate { get; set; }
+    }
+}
